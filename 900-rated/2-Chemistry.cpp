b33e67097870aa3e3149d6257131e098d2618a74
Divyansh <officialdslc1552005@gmail.com>
2025-07-24 20:45:50 +0530

    did 2nd question of 900-rated Chemistry contest

--- 900-rated/2-Chemistry.cpp
+++ 900-rated/2-Chemistry.cpp
@@ -1,40 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool is_palindrome(const string & s){
-    int start = 0 , end = s.size() - 1;
-    while(start <= end){
-        if(s[start] != s[end]) return false;
-        start++;
-        end--;
-    }
-
-    return true;
-}
-
-int main(){
-    int t;
+int main()
+{
+    long long t;
     cin >> t;
-    while (t--){
-        int n , k;
+    while (t--)
+    {
+        long long n, k;
         cin >> n >> k;
         string s;
         cin >> s;
-        
-        // case without removing any character
-        if(k == 0) {
-            if(is_palindrome(s)){
-                cout << "Yes" << endl;
-                continue;
-            }
-            else {
-                cout << "No" << endl;
-                continue;
-            }
-        }
 
+        vector<int> frequency_of_characters(26, 0);
+        for (int i = 0; i < n; i++) frequency_of_characters[s[i] - 'a']++;
+
+        long long odd_frequency = 0;
+        for (int i = 0; i < 26; i++) odd_frequency += (frequency_of_characters[i] % 2); // this freq[i] % 2 will give eiither 0 or 1 , 1 for odd , 0 for 1
+        // also in a palindrome , if length is even , all characters should be even in count , for odd length , all char except 1 will even count and that one will be odd count.
+        // so logic we can use it that if total odd count aale char <= k+1 hai , toh unko remove krke banaya jaa skta hai paldinrome  , also k + 1 islie coz k hata skte hai and + 1 for palindrome permites 1 char with odd count
         
 
+        if (odd_frequency > k + 1) cout << "NO" << endl;
+        else cout << "YES" << endl;
     }
     return 0;
 }
+ 
