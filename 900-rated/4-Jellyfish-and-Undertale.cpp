fe85b8bd58a3e1fab0c0e4092c93347aa7256821
Divyansh <officialdslc1552005@gmail.com>
2025-07-26 18:42:20 +0530

    ques-4

---
+++ 900-rated/4-Jellyfish-and-Undertale.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while (t--){
+        int a,b,n;
+        cin >> a>> b >> n;
+
+        vector<int> v(n); // tools timer array
+        
+        for (int i = 0; i < n; i++) cin >> v[i];
+        
+        
+
+    }
+    return 0;
+}
